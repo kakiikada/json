76b5f124ef00b2c4b6790fdc5a5ea8bff01cd027
kakiikada <kakiikada@gmail.com>
2018-01-25 13:45:01 +0900

    日付情報の追加

--- index.php
+++ index.php
@@ -9,40 +9,54 @@
 </head>
 <body>
 <?php require 'header.php' ?>
-<div class="container">
-  <form action="index.php" method="post">
-    <input type="text" name="comment" placeholder="コメントを入力してください">
-    <input type="submit" value="投稿する">
-  </form>
-  <hr>
   <?php
   $file = 'json.txt';
+  $file_day = 'day.txt';
+  //保存コメントデータの読み込み
   if(file_exists($file)){
     $message = json_decode(file_get_contents($file));
   }
+  if(file_exists($file_day)){
+    $day_date = json_decode(file_get_contents($file_day));
+  }
+  //コメント投稿されてるor空文字じゃないなら、コメント・日時をファイルに書き込む
   if(isset($_POST['comment']) && $_POST['comment'] != null){
     $message[] = $_POST['comment'];
+    $today = getdate();
+    //投稿日時は文字列として保存。タイムスタンプに変換して保存したほうが良いのかもしれないけど妥協。
+    $day_date[] = "$today[year]年$today[mon]月$today[mday]日 $today[hours]時$today[minutes]分";
   }elseif(!isset($_POST['delete'])){
     echo '<p>コメント未入力は投稿できません</P>';
   }
   ?>
   <?php
-  for($i = 0; $i < 100; $i++ ){
+  for($i = 0; $i < count($message); $i++ ){ //フラグがあればデータを消す処理
     if(isset($_POST["id=$i"])){
       array_splice($message,$i,1);
+      array_splice($day_date,$i,1);
     }
   }
   file_put_contents($file,json_encode($message));//ファイル書き込み
+  file_put_contents($file_day,json_encode($day_date));
   ?>
-  <?php
-  $id = 0;
-  ?>
-  <?php $message_reverse = array_reverse($message) ?>
+  <?php $id = 0; ?>
+  <?php $message_reverse = array_reverse($message); ?><!--最新を上に表示するため、配列を逆にする-->
+  <?php $day_date_reverse = array_reverse($day_date); ?>
+  <!--コメント入力部分開始-->
+  <div class="container">
+    <form class = "push"  action="index.php" method="post">
+      <input class = "input" type="text" name="comment" placeholder="コメントを入力してください">
+      <input class = "submit" type="submit" value="投稿する">
+    </form>
+    <hr>
+  <!--コメント入力部分終わり-->
   <?php foreach($message_reverse as $row): ?><!--書き出し-->
-    <p><?php echo htmlspecialchars($row); ?></p>
+    <p class = "log"><?php echo htmlspecialchars($row); ?></p>
     <form action="index.php" method="post">
+      <!--コメを消去するフラグ（配列を逆にしているので、補正をかけている）-->
       <input type="hidden" name="id=<?php echo count($message)- $id -1; ?>">
-      <input type="hidden" name="delete">
+      <input type="hidden" name="delete"><!--消去時にコメ未入力文章を出さないためにフラグを立てる-->
+      <p class = 'day-text'>投稿日時：<?php echo $day_date_reverse[$id]; ?></p>
       <input type="submit" value="消去">
     </form>
     <hr>
